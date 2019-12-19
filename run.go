b608279f14909e77ab62c1c6ea8bd4d9fac2d2f9
acjzz <jordiarnavat@gmail.com>
2019-12-19 05:45:33 +0400

    os signal interrupt handler implemented

--- run.go
+++ run.go
@@ -3,6 +3,8 @@ package main
 import (
 	gofka "./src"
 	"context"
+	"os"
+	"os/signal"
 )
 
 func main(){
@@ -14,4 +16,10 @@ func main(){
 	c.Run()
 	p := t.NewProducer()
 	p.Publish(gofka.NewInternalMessage("Testing"))
+	// Wait for SIGINT.
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
+	// Shutdown. Cancel application context will kill all attached tasks.
+	cancel()
 }
